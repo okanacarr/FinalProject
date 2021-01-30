6dc878e23802dbdbd83ef3fc751a183c195f42f1
Okan <okanacar120@gmail.com>
2021-01-31 00:44:58 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstracts;
+using DataAccess.Abstracts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+
+
+            //İş kodları
+            // kural bir iş sınıfı baska bir sınıfı new'lememeli
+            //InMemoryProductDal ınMemoryProductDal = new InMemoryProductDal(); // hata, bağımlılık olur...
+
+            // bu sekilde IProductDal implemente etmiş tüm class'lar 
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Abstracts/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstracts
+{
+    //IEntity impelemt eden class bir veritabanı tablosudur. (Product, Category)
+    // veritabani nesnesi demek icin class'lari isaretleyecegiz.
+
+    public interface IEntity
+    {
+
+    }
+}
